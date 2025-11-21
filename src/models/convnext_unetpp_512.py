ab63a69f8ca268d75ea49976950f5c9428b83a6c
summerness <chengqihang@gmail.com>
2025-11-21 18:33:27 +1300

    修改为可训练参数

--- src/models/convnext_unetpp_512.py
+++ src/models/convnext_unetpp_512.py
@@ -4,8 +4,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from utils import safe_timm_create
-from self_correlation import SelfCorrelationBlock
+from models.utils import safe_timm_create
+from models.self_correlation import SelfCorrelationBlock
 
 
 class ConvBlock(nn.Module):
